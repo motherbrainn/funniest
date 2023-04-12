b67633a0ec27d489c42aab3bae30cc118b3229e3
Aaron Ehresman <aaron.ehr@gmail.com>
2023-04-11 22:03:59 -0700

    add support for related images

--- server/index.js
+++ server/index.js
@@ -58,6 +58,7 @@ const createImages = async () => {
     data: newImages,
     skipDuplicates: true,
   });
+  await persistNewImages(data);
 };
 
 const numberOfItemsToDrop = async (percentageOfImagesToDrop) => {
@@ -77,7 +78,7 @@ const cronJob = async () => {
   const amountToDrop = await numberOfItemsToDrop(0.7);
   const imageThreshold = 200;
   await dropImages(amountToDrop);
-  await addImages(imageThreshold);
+  await addImages(imageThreshold, true);
   await resetVotes();
 };
 
@@ -94,46 +95,103 @@ const resetVotes = async () => {
   });
 };
 
-const addImages = async (imageThreshold) => {
+//get top images
+const getTopImages = async (numberOfTopImages) =>
+  await prisma.funny_images.findMany({
+    orderBy: [{ votes: "desc" }],
+    take: numberOfTopImages,
+  });
+
+const getRelatedImages = async (imageId, numberOfRelatedImages) => {
+  return await gf.related(imageId, {
+    limit: numberOfRelatedImages,
+  });
+};
+
+const persistNewImages = async (data) => {
+  const newImages = data.map((image) => {
+    const imageUrl = image.images.downsized_large.url;
+    const imageId = image.id;
+    return { image_url: imageUrl, image_id: imageId };
+  });
+
+  await prisma.funny_images.createMany({
+    data: newImages,
+    skipDuplicates: true,
+  });
+};
+
+const addImages = async (imageThreshold, addRelatedImages) => {
   const gf = new GiphyFetch(giphyApiKey);
 
-  const searchString = "funny, trending, dogs, meme";
-  const searchOptions = { lang: "en", rating: "pg" };
+  let numberOfCurrentlyPersistedImages;
 
-  //check how many total images are return from search string before doing real search at scale
-  const {
-    pagination: { total_count: totalCount },
-  } = await gf.search(searchString, { limit: 1, ...searchOptions });
+  //only add related images on first iteration, it gets too messy with recursion otherwise
+  if (addRelatedImages === true) {
+    const relatedSearchLimit = Math.floor((1 / 10) * imageThreshold);
 
-  //4999 is max offset from API
-  const maxOffset = totalCount > 4999 ? 4999 : totalCount;
+    //retrieve top 5 images
+    const topImages = await getTopImages(5);
 
-  //giphy search returns results in consistent order. use random offset to get randomized new images
-  const randomOffset = getRandomIntInclusive(0, maxOffset);
+    //get related images
+    for (image of topImages) {
+      //check how many total images are return from search string before doing real search at scale
+      const {
+        pagination: { total_count: totalCount },
+      } = await getRelatedImages(image.image_id, 1);
 
-  //check how many images are currently persisted
-  const numberOfCurrentlyPersistedImages = await prisma.funny_images.count();
+      //150 is max offset from API
+      const maxOffset = totalCount > 150 ? 150 : totalCount;
 
+      //giphy returns results in consistent order. use random offset to get randomized new images
+      const randomOffset = getRandomIntInclusive(0, maxOffset);
+
+      const { data } = await getRelatedImages(
+        image.image_id,
+        relatedSearchLimit,
+        {
+          offset: randomOffset,
+        }
+      );
+
+      await persistNewImages(data);
+
+      // //check how many images are currently persisted, if above threshold, break
+      numberOfCurrentlyPersistedImages = await prisma.funny_images.count();
+
+      if (numberOfCurrentlyPersistedImages > imageThreshold) {
+        break;
+      }
+    }
+  } else {
+    numberOfCurrentlyPersistedImages = await prisma.funny_images.count();
+  }
+
+  //check if we are still under threshold, if we are, add more images, unrelated to last top images
   if (numberOfCurrentlyPersistedImages < imageThreshold) {
+    const searchString = "funny, trending, dogs, meme";
+    const searchOptions = { lang: "en", rating: "pg" };
+
+    //check how many total images are return from search string before doing real search at scale
+    const {
+      pagination: { total_count: totalCount },
+    } = await gf.search(searchString, { limit: 1, ...searchOptions });
+
+    //4999 is max offset from API
+    const maxOffset = totalCount > 4999 ? 4999 : totalCount;
+
+    //giphy search returns results in consistent order. use random offset to get randomized new images
+    const randomOffset = getRandomIntInclusive(0, maxOffset);
+
     const { data } = await gf.search(searchString, {
       limit: 50,
       offset: randomOffset,
       ...searchOptions,
     });
+    await persistNewImages(data);
 
-    const newImages = data.map((image) => {
-      const imageUrl = image.images.downsized_large.url;
-      const imageId = image.id;
-      return { image_url: imageUrl, image_id: imageId };
-    });
-
-    await prisma.funny_images.createMany({
-      data: newImages,
-      skipDuplicates: true,
-    });
-
-    //keep adding images until we reach threshold
-    addImages(imageThreshold);
+    //keep adding images until we reach threshold, only add related images on first iteration
+    addImages(imageThreshold, false);
   }
   return;
 };
@@ -180,6 +238,14 @@ app.get("/test", async (req, res) => {
     ...searchOptions,
   });
   console.log(data[0].id);
+  res.send("done");
+});
+
+app.get("/related", async (req, res) => {
+  // const { data: gifs } = await gf.related("mUrBX1TF0kCRi", { limit: 10 });
+  // console.log(gifs);
+  console.log(await getRelatedImages("mUrBX1TF0kCRi", 2));
+  //console.log(await getTopImages(5));
   res.send("done");
 });
 
