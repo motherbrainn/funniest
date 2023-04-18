7e7a356254c6ae8aa5a32aa858618e2636f9808d
Aaron Ehresman <aaron.ehr@gmail.com>
2023-04-17 21:03:46 -0700

    display both images at once

--- client/src/components/FunnyImage.tsx
+++ client/src/components/FunnyImage.tsx
@@ -4,11 +4,13 @@ import useMediaQuery from "@mui/material/useMediaQuery";
 interface FunnyImageProps {
   clickHandler: () => void;
   imageUrl: string;
+  imageLoadCallback: () => void;
 }
 
 export const FunnyImage = ({
   clickHandler,
   imageUrl,
+  imageLoadCallback,
 }: FunnyImageProps): ReactElement => {
   const mobile = useMediaQuery("(min-width:600px)");
 
@@ -21,6 +23,7 @@ export const FunnyImage = ({
         alt="funny-image"
         height={mobile ? 500 : 150}
         width={mobile ? 500 : 150}
+        onLoad={imageLoadCallback}
       />
     </div>
   );
--- client/src/components/ImageChoice.tsx
+++ client/src/components/ImageChoice.tsx
@@ -1,22 +1,44 @@
 import { ImageChoiceObject } from "@/pages/ImageContainer";
-import { ReactElement } from "react";
+import styled from "@emotion/styled";
+import { ReactElement, useState } from "react";
 import FunnyImage from "./FunnyImage";
 
 export interface ImageChoiceProps {
   images: ImageChoiceObject;
 }
 
+enum Side {
+  LEFT = "left",
+  RIGHT = "right",
+}
+
 const ImageChoice = ({ images }: ImageChoiceProps): ReactElement => {
+  const [leftImageLoaded, setLeftImageLoaded] = useState(false);
+  const [rightImageLoaded, setRightImageLoaded] = useState(false);
+
+  const StyledImageContainer = styled("div")({
+    display: leftImageLoaded && rightImageLoaded ? "visible" : "none",
+  });
+
+  const handleClick = (side: Side) => {
+    side === "left" ? images.left.clickHandler() : images.right.clickHandler();
+    setRightImageLoaded(false);
+  };
+
   return (
-    <div className="image-container">
-      <FunnyImage
-        clickHandler={() => images.left.clickHandler()}
-        imageUrl={images.left.imageUrl}
-      />
-      <FunnyImage
-        clickHandler={() => images.right.clickHandler()}
-        imageUrl={images.right.imageUrl}
-      />
+    <div>
+      <StyledImageContainer className="image-container">
+        <FunnyImage
+          clickHandler={() => handleClick(Side.LEFT)}
+          imageUrl={images.left.imageUrl}
+          imageLoadCallback={() => setLeftImageLoaded(true)}
+        />
+        <FunnyImage
+          clickHandler={() => handleClick(Side.RIGHT)}
+          imageUrl={images.right.imageUrl}
+          imageLoadCallback={() => setRightImageLoaded(true)}
+        />
+      </StyledImageContainer>
     </div>
   );
 };
