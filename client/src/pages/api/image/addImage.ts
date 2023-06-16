ec132a720b59c1ce035cb5715bbe71ac43cdfc80
Aaron Ehresman <aaron.ehr@gmail.com>
2023-06-16 09:44:36 -0700

    comment out test code

--- client/src/pages/api/image/addImage.ts
+++ client/src/pages/api/image/addImage.ts
@@ -5,16 +5,17 @@ import prisma from "../../../../lib/prisma";
 // POST /api/post
 // Required fields in body: title
 // Optional fields in body: content
-export default async function handle(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const { title, content } = req.body;
 
-  const result = await prisma.funny_images.create({
-    data: {
-      image_url: "www.cool.com",
-    },
-  });
-  res.json(result);
-}
+// export default async function handle(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   const { title, content } = req.body;
+
+//   const result = await prisma.funny_images.create({
+//     data: {
+//       image_url: "www.cool.com",
+//     },
+//   });
+//   res.json(result);
+// }
